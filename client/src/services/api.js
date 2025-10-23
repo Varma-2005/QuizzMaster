6e473e73777a6130c59b07480f1d38f1f503e0ad
Varma-2005 <23071a12h4@vnrvjiet.in>
2025-10-23 19:27:38 +0530

    "Backend Deploy"

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,13 +1,20 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
+// Automatically choose base URL depending on environment
+const baseURL =
+  window.location.hostname === 'localhost'
+    ? 'http://localhost:3000'
+    : 'https://quizzmaster-h9rv.onrender.com';
+
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    'Content-Type': 'application/json',
+  },
 });
 
+// Request Interceptor: attach token if available
 api.interceptors.request.use(
   (config) => {
     const token = Cookies.get('token');
@@ -16,11 +23,10 @@ api.interceptors.request.use(
     }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
+// Response Interceptor: handle 401 and redirect to login
 api.interceptors.response.use(
   (response) => response,
   (error) => {
